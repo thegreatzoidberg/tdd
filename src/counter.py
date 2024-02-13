524e656d9c88ea45e66e32d8914a9a04de23d303
thegreatzoidberg <147963318+thegreatzoidberg@users.noreply.github.com>
2024-02-12 20:06:15 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -52,6 +52,6 @@ def delete_counter(name):
     global COUNTERS
     if name not in COUNTERS:
         return {"Message": f"Counter {name} does not exist"}, status.HTTP_204_NO_CONTENT
-    x = COUNTERS[name]
+    x = {name: COUNTERS[name]}
     del COUNTERS[name]
     return {x, status.HTTP_204_NO_CONTENT}
