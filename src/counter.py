497ba6fbf48773489925bd486913c1d8116a6fb0
thegreatzoidberg <147963318+thegreatzoidberg@users.noreply.github.com>
2024-02-12 17:46:28 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -11,8 +11,8 @@ COUNTERS = {}
 # specify the variable in route <name>
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
-@app.route('/counters/<name>', methods=['POST'])
 
+@app.route('/counters/<name>', methods=['POST'])
 def create_counter(name): 
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
