aca26d6efca9b6f5516b01b355428d02ebe9903c
thegreatzoidberg <147963318+thegreatzoidberg@users.noreply.github.com>
2024-02-12 17:50:36 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -12,11 +12,12 @@ COUNTERS = {}
 # let Flask know that the only methods that is allowed to called
 # on this function is "POST".
 
+
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
     """Create a counter"""
     app.logger.info(f"Request to create counter: {name}")
-    global COUNTERS
+    global COUNTERS 
     if name in COUNTERS: 
         return {"Message":f"Counter {name} already exists"}, status.HTTP_409_CONFLICT
 
