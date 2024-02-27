b79a0969170c847f90cc6f48f01f88ecfda59933
thegreatzoidberg <147963318+thegreatzoidberg@users.noreply.github.com>
2024-02-26 18:29:14 -0800

    Update counter.py

--- src/counter.py
+++ src/counter.py
@@ -1,17 +1,11 @@
-# we need to import the file that contains the status codes
-from src import status
-
+"""Module docstring for counter.py"""
+# Import third-party modules before first-party modules
 from flask import Flask
+from src import status
 
 app = Flask(__name__)
 
 COUNTERS = {}
-
-# We will use the app decorator and create a route called slash counters.
-# specify the variable in route <name>
-# let Flask know that the only methods that is allowed to called
-# on this function is "POST".
-
 
 @app.route('/counters/<name>', methods=['POST'])
 def create_counter(name):
@@ -24,7 +18,6 @@ def create_counter(name):
     COUNTERS[name] = 0
     return {name: COUNTERS[name]}, status.HTTP_201_CREATED
 
-
 @app.route('/counters/<name>', methods=['PUT'])
 def update_counter(name):
     """Update a counter"""
@@ -34,7 +27,6 @@ def update_counter(name):
 
     return {name: COUNTERS[name]}, status.HTTP_200_OK
 
-
 @app.route('/counters/<name>', methods=['GET'])
 def read_counter(name):
     """Read a counter"""
@@ -43,7 +35,6 @@ def read_counter(name):
     x = {name: COUNTERS[name]}
 
     return x, status.HTTP_200_OK
-
 
 @app.route('/counters/<name>', methods=['DELETE'])
 def delete_counter(name):
